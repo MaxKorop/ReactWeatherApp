bb1def886970879b1b8e1b64c54903495fbb64b2
Max Korop <makskorop99@gmail.com>
2023-07-01 15:09:43 +0300

    Added API key and base

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 
+const api = {
+  key: "bf08ada48295eada7763b2cd2b880256",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
 
   return (
